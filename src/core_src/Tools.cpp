ff4d483b4150d1d28a06abae366ff1a5ef21a8e2
Ernesto <paiser@lbl.gov>
2024-01-05 16:25:47 -0800

    Initial commit

---
+++ src/core_src/Tools.cpp
@@ -0,0 +1,22 @@
+/*
+ * Tools.cpp
+ *
+ *  Created on: Apr 25, 2013
+ *      Author: kowalisz
+ */
+
+
+#include "Tools.h"
+
+vector<string> string2vector(string &str, const char* tokens)
+{
+	std::vector<std::string> fields;
+	std::stringstream ss(str);
+	std::string item;
+	while(std::getline(ss, item, *tokens))
+	{
+		fields.push_back(item);
+	}
+
+	return fields;
+}
